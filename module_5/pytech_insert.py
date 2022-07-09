3eb7e8909e0864c7e45670560cc205965005d760
Werner Alberts <werneralberts01@gmail.com>
2022-07-09 13:08:00 -0500

    adding records to database

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,56 @@
+from pydoc import doc
+from xml.etree.ElementPath import find
+from pymongo import InsertOne, MongoClient
+
+
+from pymongo import MongoClient
+import pymongo.mongo_client
+
+url = "mongodb+srv://admin:admin@cluster0.w5mlyq8.mongodb.net/?retryWrites=true&w=majority";
+
+client = MongoClient(url);
+db = client.pytech
+
+pytech = db["students"]
+
+records = [
+    {
+        "student_id": "1007",
+        "first_name": "Jake",
+        "last_name": "Wilson"
+    },
+    {
+        "student_id": "1008",
+        "first_name": "Johhny",
+        "last_name": "Marley"
+    },
+    {
+        "student_id": "1009",
+        "first_name": "Jill",
+        "last_name": "Barnet"
+    }
+]
+
+print("-- INSERT STATEMENTS --")
+for record in records:
+    new_student_Id = pytech.insert_one(record).inserted_id
+    new_student_fName = record.get("first_name")
+    new_student_lName = record.get("last_name") 
+    print("Inserted student record "+str(new_student_fName)+" "+str(new_student_lName)+"into the students collection with document_id "+str(new_student_Id))
+
+docs = pytech.find()
+
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() Query --")
+
+for doc in docs:
+    print("Student ID: "+doc["student_id"])
+    print("First Name: "+doc["first_name"])
+    print("Last Name: "+doc["last_name"])
+
+findRecord = pytech.find_one({"student_id": "1008"})
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find_one() Query --")
+
+for record in findRecord:
+    print("Student ID: "+findRecord["student_id"])
+    print("First Name: "+findRecord["first_name"])
+    print("Last Name: "+findRecord["last_name"])
